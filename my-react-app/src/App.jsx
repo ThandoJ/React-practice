cc9bf90c677c19b1fb0aa3e9fcaa5193a25d9128
ThandoJ <jasopthando0@gmail.com>
2026-02-17 15:51:20 +0200

    Layout

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+     
+    </div>
+  );
+}
+
+export default App;
